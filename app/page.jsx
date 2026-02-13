9b2467fb1b3178bfbdacb7ac865426bfc1541a06
Remo Lalata <remo@winkreports.com>
2026-02-13 18:20:43 +0800

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Firstpage from "./(homes)/home-1/page";
+
+export const metadata = {
+  title: "GR8 Escapes Travel & Tours",
+  description:
+    "Achieving your dream destination with hassle-free service of Gr8 Escapes Travel & Tours!",
+};
+
+export default function page() {
+  return (
+    <>
+      <Firstpage />
+    </>
+  );
+}
---
+++ components/homes/heros/Hero1.jsx
@@ -0,0 +1,201 @@
+"use client";
+
+import Calender from "@/components/common/dropdownSearch/Calender";
+import Location from "@/components/common/dropdownSearch/Location";
+import TourType from "@/components/common/dropdownSearch/TourType";
+import Image from "next/image";
+import React, { useEffect, useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+export default function Hero1() {
+  const router = useRouter();
+  const [currentActiveDD, setCurrentActiveDD] = useState("");
+  const [location, setLocation] = useState("");
+  const [calender, setCalender] = useState("");
+  const [tourType, setTourType] = useState("");
+  useEffect(() => {
+    setCurrentActiveDD("");
+  }, [location, calender, tourType, setCurrentActiveDD]);
+
+  const dropDownContainer = useRef();
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (
+        dropDownContainer.current &&
+        !dropDownContainer.current.contains(event.target)
+      ) {
+        setCurrentActiveDD("");
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return (
+    <section className="hero -type-1">
+      <div className="hero__bg">
+        <Image
+          width={1800}
+          height={560}
+          src="/img/hero.webp"
+          alt="image"
+          style={{ objectPosition: "center bottom" }}
+        />
+        <Image
+          width="1800"
+          height="40"
+          src="/img/hero/1/shape.svg"
+          alt="image"
+          style={{ height: "auto" }}
+        />
+      </div>
+
+      <div className="container">
+        <div className="row justify-center">
+          <div className="col-xl-8 col-lg-10">
+            <div className="hero__content">
+              <h1
+                data-aos={"fade-up"}
+                data-aos-delay="100"
+                className="hero__title"
+              >
+                Your Dream Destination, Made Hassle-Free
+              </h1>
+
+              <p
+                data-aos={"fade-up"}
+                data-aos-delay="300"
+                className="hero__text"
+              >
+                From flights and hotels to tours and transfers, Gr8 Escapes
+                Travel & Tours brings you complete travel packages for
+                unforgettable local and international getaways.
+              </p>
+
+              <div
+                ref={dropDownContainer}
+                data-aos={"fade-up"}
+                data-aos-delay="300"
+                className="mt-60 md:mt-35"
+              >
+                <div className="searchForm -type-1">
+                  <div className="searchForm__form">
+                    <div className="searchFormItem js-select-control js-form-dd">
+                      <div
+                        className="searchFormItem__button"
+                        onClick={() =>
+                          setCurrentActiveDD((pre) =>
+                            pre == "location" ? "" : "location",
+                          )
+                        }
+                      >
+                        <div className="searchFormItem__icon size-50 rounded-12 border-1 flex-center">
+                          <i className="text-20 icon-pin"></i>
+                        </div>
+                        <div className="searchFormItem__content">
+                          <h5>Where</h5>
+                          <div className="js-select-control-chosen">
+                            {location ? location : "Search destinations"}
+                          </div>
+                        </div>
+                      </div>
+
+                      <Location
+                        setLocation={setLocation}
+                        active={currentActiveDD === "location"}
+                      />
+                    </div>
+
+                    <div className="searchFormItem js-select-control js-form-dd js-calendar">
+                      <div
+                        className="searchFormItem__button"
+                        onClick={() =>
+                          setCurrentActiveDD((pre) =>
+                            pre == "calender" ? "" : "calender",
+                          )
+                        }
+                      >
+                        <div className="searchFormItem__icon size-50 rounded-12 border-1 flex-center">
+                          <i className="text-20 icon-calendar"></i>
+                        </div>
+                        <div className="searchFormItem__content">
+                          <h5>When</h5>
+                          <div>
+                            <span className="js-first-date">
+                              <Calender
+                                active={currentActiveDD === "calender"}
+                              />
+                            </span>
+                            <span className="js-last-date"></span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="searchFormItem js-select-control js-form-dd">
+                      <div
+                        className="searchFormItem__button"
+                        onClick={() =>
+                          setCurrentActiveDD((pre) =>
+                            pre == "tourType" ? "" : "tourType",
+                          )
+                        }
+                      >
+                        <div className="searchFormItem__icon size-50 rounded-12 border-1 flex-center">
+                          <i className="text-20 icon-flag"></i>
+                        </div>
+                        <div className="searchFormItem__content">
+                          <h5>Tour Type</h5>
+                          <div className="js-select-control-chosen">
+                            {tourType ? tourType : "All tour"}
+                          </div>
+                        </div>
+                      </div>
+
+                      <TourType
+                        setTourType={setTourType}
+                        active={currentActiveDD === "tourType"}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="searchForm__button">
+                    <button
+                      onClick={() => router.push("/tour-list-1")}
+                      className="button -dark-1 bg-accent-1 text-white"
+                    >
+                      <i className="icon-search text-16 mr-10"></i>
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <p className="text-12 text-white mt-10">
+                Photo by{" "}
+                <a
+                  href="https://unsplash.com/@maxgrig?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Maksim Grigorev
+                </a>{" "}
+                on{" "}
+                <a
+                  href="https://unsplash.com/photos/a-colorful-surfboard-sitting-on-a-beach-next-to-a-life-guard-tower-u0wbwPCGlQo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Unsplash
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
